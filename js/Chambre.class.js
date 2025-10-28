4643ecd4d3ae7020bba380102ab4911756b35746
jbAfpa <jea.vau@dwwm.afpa>
2025-10-28 10:15:54 +0100

    indent 2 class

--- js/Chambre.class.js
+++ js/Chambre.class.js
@@ -1,61 +1,64 @@
 "use strict"
 // classe à metre en export si utilisatuion de live
 export class Chambre {
-    // Attribue qui constitue une chambre
-    #capacite = 0;
-    #superficie = 0;
-    #reservation;
-    // constructeur  avec les valeur qui contreuise la Chambre
-    constructor(capacite,superficie,reservation){
-      this.#setCapacite(capacite);
-      this.#setSuperficie(superficie);
-      this.setReservation(reservation);
-    }
-    // setter  
-    #setCapacite(capacite){
-      if (!capacite) throw new Error("ERR: Champs 'capacite' obligatoir");
-      if (isNaN(capacite)) throw new Error("ERR: Champs 'capacite'est un nombre");
-      if (capacite <= 0)  throw new Error("ERR: Champs 'capacite'est un nombre positif");
+  // Attribue qui constitue une chambre
+  #capacite = 0;
+  #superficie = 0;
+  #reservation;
+
+  // constructeur  avec les valeur qui contreuise la Chambre
+  constructor(capacite,superficie,reservation){
+    this.#setCapacite(capacite);
+    this.#setSuperficie(superficie);
+    this.setReservation(reservation);
+  }
+
+  // setter  
+  #setCapacite(capacite){
+    if (!capacite) throw new Error("ERR: Champs 'capacite' obligatoir");
+    if (isNaN(capacite)) throw new Error("ERR: Champs 'capacite'est un nombre");
+    if (capacite <= 0)  throw new Error("ERR: Champs 'capacite'est un nombre positif");
 
 
-      this.#capacite = capacite;
-    }
+    this.#capacite = capacite;
+  }
 
-    #setSuperficie(superficie){
-      if (!superficie) throw new Error("ERR: Champs 'superficie' obligatoir");
-      if (isNaN(superficie)) throw new Error("ERR: Champs 'superficie'est un nombre");
-      if (superficie <= 0)  throw new Error("ERR: Champs 'superficie'est un nombre positif");
+  #setSuperficie(superficie){
+    if (!superficie) throw new Error("ERR: Champs 'superficie' obligatoir");
+    if (isNaN(superficie)) throw new Error("ERR: Champs 'superficie'est un nombre");
+    if (superficie <= 0)  throw new Error("ERR: Champs 'superficie'est un nombre positif");
 
-      this.#superficie = superficie;
-    }
+    this.#superficie = superficie;
+  }
 
-    setReservation(){
+  setReservation(){
 
-    }
-    // getter pour quand on veux récup les donner des attribue de chambre 
-    getCapacite(){
-        return this.#capacite;
-    }
+  }
+  // getter pour quand on veux récup les donner des attribue de chambre 
+  getCapacite(){
+    return this.#capacite;
+  }
 
-    getSuperficie(){
-      return  this.#superficie;
-    }
+  getSuperficie(){
+    return  this.#superficie;
+  }
 
-    getReservation(){
-      return  this.#reservation;
-    }
-    // methode suplémentaire 
-    affiche(){
-        console.log("Ma superficie est de " + this.#superficie);
-        console.log("Ma capacite est de " + this.#capacite);
-        
-    }
+  getReservation(){
+    return  this.#reservation;
+  }
+  // methode suplémentaire 
+  affiche(){
+    console.log("Ma superficie est de " + this.#superficie);
+    console.log("Ma capacite est de " + this.#capacite);
+      
+  }
 
-    reserverChambre(){
+  reserverChambre(){
 
-    }
+  }
 
-    libereChambre(){
+  libereChambre(){
 
-    }
+  }
+  
 }
