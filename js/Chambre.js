5a69e503d8459425500306e244731a8ee01fabdc
jbAfpa <jea.vau@dwwm.afpa>
2025-10-28 08:36:24 +0100

    fixe indente

--- js/Chambre.js
+++ js/Chambre.js
@@ -6,41 +6,41 @@ class Chambre {
     #superficie;
     #reservation;
     // constructeur  avec les valeur qui contreuise la Chambre
-    constructor(capacite,superficie,reservation) {
+    constructor(capacite,superficie,reservation){
         this.setCapacite(capacite);
         this.setSuperficie(superficie);
         this.setReservation(reservation);
     }
     // setter  
-    setCapacite(capacite) {
+    setCapacite(capacite){
         this.#capacite = capacite;
     }
 
-    setSuperficie(superficie) {
+    setSuperficie(superficie){
         this.#superficie = superficie;
     }
 
-    setReservation() {
+    setReservation(){
 
     }
     // getter pour quand on veux récup les donner des attribue de chambre 
-    getCapacite() {
-       return this.#capacite
+    getCapacite(){
+        return this.#capacite;
     }
 
-    getSuperficie() {
-      return  this.#superficie
+    getSuperficie(){
+      return  this.#superficie;
     }
 
     getReservation(){
-      return  this.#reservation
+      return  this.#reservation;
     }
-    // action suplémentaire 
-    reserverChambre() {
+    // methode suplémentaire 
+    reserverChambre(){
 
     }
 
-    libereChambre() {
+    libereChambre(){
 
     }
 }
