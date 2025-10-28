3fa32f228dc77dc05fa3ac61d1531eec3eac752b
jbAfpa <jea.vau@dwwm.afpa>
2025-10-28 09:27:47 +0100

    attente de teste de superficie merge dam

--- js/Chambre.class.js
+++ js/Chambre.class.js
@@ -1,23 +1,27 @@
 "use strict"
 // classe à metre en export si utilisatuion de live
-class Chambre {
+export class Chambre {
     // Attribue qui constitue une chambre
     #capacite;
-    #superficie;
+    #superficie = 0;
     #reservation;
     // constructeur  avec les valeur qui contreuise la Chambre
     constructor(capacite,superficie,reservation){
-        this.setCapacite(capacite);
-        this.setSuperficie(superficie);
-        this.setReservation(reservation);
+      this.setCapacite(capacite);
+      this.setSuperficie(superficie);
+      this.setReservation(reservation);
     }
     // setter  
     setCapacite(capacite){
-        this.#capacite = capacite;
+      this.#capacite = capacite;
     }
 
-    setSuperficie(superficie){
-        this.#superficie = superficie;
+    #setSuperficie(superficie){
+      if (!superficie) throw new Error("ERR: Champs 'superficie' obligatoir");
+      if (isNaN(superficie)) throw new Error("ERR: Champs 'superficie'est un nombre");
+      if (superficie <= 0)  throw new Error("ERR: Champs 'superficie'est un nombre positif");
+
+      this.#superficie = superficie;
     }
 
     setReservation(){
@@ -36,6 +40,10 @@ class Chambre {
       return  this.#reservation;
     }
     // methode suplémentaire 
+    affiche(){
+        console.log("Ma superficie est de " + this.#superficie)
+    }
+
     reserverChambre(){
 
     }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
     "use strict"
 
+    import { Chambre } from "./Chambre.class.js";
+
     // faire un hotel dans un tableau de 10chambre
     let tHotel = [1,2,3,4,5,6,7,8,9,10]
 
 
+     // SCENARIOS ALTERNATIFS - TESTS
+    try {
+        //let ch1 = new Chambre();
+        //let ch2 = new Chambre("toto");
+        //let ch3 = new Chambre(-10);
+
+        // SCENARIO NOMINAL
+        let ch4 = new Chambre(10);
+
+        //console.log(ch4);
+        ch4.affiche();
+
+    } catch(err) {
+        console.error(err.message);
+    }
+
+    console.log("Mon programme continue");
